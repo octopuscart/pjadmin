b388a22e072c1ce021fa1d167f44f74ac1a6ef3f
octopuscart <octopuscartltd@gmail.com>
2022-11-14 01:42:52 +0530

    add files

--- application/config/configuration_db.php
+++ application/config/configuration_db.php
@@ -1,13 +1,7 @@
 <?php
-<<<<<<< HEAD
 //$activedb = "j2k5e6r5_rahman";
 $activedb = "pjspaul_paul";
 $activedbusername = "pjspaul_paul";
 $activedbpassword = "z0Hg2oYp";
-=======
-$activedb = "j2k5e6r5_rahman";
-//$activedb = "j2k5e6r5_royaltailor";
-$activedbusername = "j2k5e6r5_octopus";
-$activedbpassword = "India$2017";
->>>>>>> fb79d355752cc66e6539890c6033fdbe80659ddc
+
 ?>
